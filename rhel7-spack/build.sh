9efe62de28ce5c194080ca6f056f4b38cff5532e
Chris Blanton <Chris.Blanton@lscmdb02-d.gfdl.noaa.gov>
2021-09-02 17:37:59 -0400

    Add Dockerfile and build.sh for Spack installation on top of ecpe4s/rhel8-runner-x86_64:2021-09-01-gcc11.2

---
+++ rhel7-spack/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+BUILD_DATE=$(printf '%(%Y-%m-%d)T' -1)
+BUILD_TAG=${BUILD_TAG:-${BUILD_DATE}}
+BUILD_REPO=$(git config --get remote.origin.url)
+BUILD_REPO_REF=$(git log -1 --format="%H")
+
+REGISTRY=${REGISTRY:-chrisblanton}
+OUTPUT_IMAGE="${REGISTRY}/rhel8-spack-x86_64:${BUILD_TAG}-gcc11.2"
+
+SPACK_REPO=https://github.com/spack/spack.git
+SPACK_REPO_REF=v0.16.2
+
+docker build \
+  --build-arg BUILD_DATE="${BUILD_DATE}" \
+  --build-arg BUILD_REPO="${BUILD_REPO}" \
+  --build-arg BUILD_REPO_REF="${BUILD_REPO_REF}" \
+  --build-arg SPACK_REPO="${SPACK_REPO}" \
+  --build-arg SPACK_REPO_REF="${SPACK_REPO_REF}" \
+  -t "${OUTPUT_IMAGE}" .
