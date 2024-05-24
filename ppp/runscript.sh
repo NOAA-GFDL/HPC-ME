fc3f54fdb93ee8cf9ca9b0906ca2f1bcda022d5e
Dana Singh <Dana.Singh@noaa.gov>
2024-05-24 10:42:37 -0400

    #26 Add condition for when on cluster environment vs not
    - when hostname has "cluster" copy contents of pp repo into cylc-src dir
    - when hostname does not have "cluster", use `fre pp checkout ...`
    - condition added because gitlab cloning did not work in cloud (github works though)

--- ppp/runscript.sh
+++ ppp/runscript.sh
@@ -13,27 +13,29 @@ conda activate /opt/conda/cylc-flow-tools
 
 # Update fre-cli (if new container not built with updated version)
 ## TO-DO?: automate rebuilding container when there is an updated fre-cli and updated pp repo version
-conda update fre-cli
+#conda update fre-cli
 
 # User input
 echo Please Enter Experiment Name:
-read -r expname
+#read -r expname
 
 echo Please Enter Platform:
-read -r plat
+#read -r plat
 
 echo Please Enter Target:
-read -r targ
+#read -r targ
 
 echo Please Enter Path to yaml file:
-read -r yamlfile
+#read -r yamlfile
+
+#ls ${HOME}
 
 ##### FOR OWN DEBUGGIN PURPOSES #####
 # Usually user can input this since it varies per experiment - but for our debugging purposes, define here
-#expname='am5_c96L65_amip'
-#plat='gfdl.ncrc5-intel22-classic'
-#targ='prod-openmp'
-#yamlfile="/${HOME}/pp.yaml"
+expname='am5_c96L65_amip'
+plat='gfdl.ncrc5-intel22-classic'
+targ='prod-openmp'
+yamlfile="${HOME}/pp.yaml"
 ##### FOR OWN DEBUGGIN PURPOSES #####
 
 # Define name of pp run
@@ -41,56 +43,65 @@ name=$expname"__"$plat"__"$targ
 
 # Create cylc-src directory (usually done with fre pp checkout - but gitlab has issues in cloud)
 cylcsrcdir="/mnt/cylc-src"
-if [ -d  $cylcsrcdir ]; then
-  echo "Directory exists."
-  rm -rf $cylcsrcdir
-  mkdir -p $cylcsrcdir/${name}
+if [[ $(echo $(hostname)) == *cluster* ]]; then
+    if [ -d  $cylcsrcdir ]; then
+        echo "cylc-src directory exists. Removing"
+        rm -rf $cylcsrcdir
+        mkdir -p $cylcsrcdir/${name}
+        # Copy contents of post-processing directory into cylc-src/name
+        cp -r /mnt2/. $cylcsrcdir/${name}
+    else
+        mkdir - $cylcsrcdir/${name}
+        # Copy contents of post-processing directory into cylc-src/name
+        cp -r /mnt2/. $cylcsrcdir/${name}
+    fi
 else
-  mkdir - $cylcsrcdir/${name}
+    if [ -d  $cylcsrcdir ]; then
+        echo "cylc-src directory exists. Removing."
+        rm -rf $cylcsrcdir
+        fre pp checkout -e ${expname} -p ${plat} -t ${targ}
+    else
+        fre pp checkout -e ${expname} -p ${plat} -t ${targ}
+    fi
 fi
-
-# Copy contents of post-processing directory into cylc-src/name
-cp -r /mnt2/. $cylcsrcdir/${name}
-
 
 ##### FRE-CLI STEPS #####
 ## Checkout 
-# fre pp checkout -e [exp] -p [plat] -t [targ] 
+#fre pp checkout -e [exp] -p [plat] -t [targ] 
 
 ## SET-UP
 #Configure
 fre pp configure-yaml -e ${expname} -p ${plat} -t ${targ} -y ${yamlfile}
 
 # Validate
-#fre pp validate -e ${expname} -p ${plat} -t ${targ}
+# fre pp validate -e ${expname} -p ${plat} -t ${targ}
 
 # Install
 # experiment installed if not previously installed
 # if installed - experiments is stopped, cleaned, and installed again
 if [ -d /mnt/cylc-run/${name} ]; then
-  echo "${name} previously installed"
+    echo "${name} previously installed"
 
-  # Check if there is a workflow running
-  cws=$(fre pp status -e ${expname} -p ${plat} -t ${targ})
-  echo $cws
+    # Check if there is a workflow running
+    cws=$(fre pp status -e ${expname} -p ${plat} -t ${targ})
+    echo $cws
 
-  # running experiment?
-  if [ ! -z "${cws}" ] && echo ${cws} | grep -q -e "running" -e "failed" -e "waiting" -e "submitted" ; then 
-    cylc stop --now --now ${name}
-    echo "${name} STOPPED"
-    sleep 5
-    cylc clean ${name}
-  fi 
-
+    # running experiment?
+    if [ ! -z "${cws}" ] && echo ${cws} | grep -q -e "running" -e "failed" -e "waiting" -e "submitted" ; then 
+      cylc stop --now --now ${name}
+      echo "${name} STOPPED"
+      sleep 5
+      cylc clean ${name}
+    fi 
 else
-  echo "\n${name} not installed\n"
+    echo "\n${name} not installed\n"
 fi
 
 # Install
 fre pp install -e ${expname} -p ${plat} -t ${targ}
 
 # RUN
-fre pp run -e ${expname} -p ${plat} -t ${targ}
+#fre pp run -e ${expname} -p ${plat} -t ${targ}
 
 # POST CHECK
 #fre pp status -e ${expname} -p ${plat} -t ${targ}
